bae44794a1ae7802a0a5c173f66a04bf279539af
Kalpana98 <kalpana360395@gmail.com>
2024-02-13 01:50:26 +0530

    react-redux implementation

--- main.js
+++ main.js
@@ -1,36 +1,27 @@
 import { store1 } from './redux/store.js';
+import { decrement, increment, reset } from './redux/slice.js';
+
+const { useSelector, useDispatch, Provider } = ReactRedux;
 
 function App() {
-	const [counter, setCounter] = React.useState(store1.getState());
-
-	function increment() {
-		const action = { type: 'INC', payload: 10 };
-		store1.dispatch(action);
-	}
-
-	function decrement() {
-		const action = { type: 'DEC', payload: 10 };
-		store1.dispatch(action);
-	}
-
-	const unsubscribe = store1.subscribe(() => setCounter(store1.getState()));
-
-	function reset() {
-		unsubscribe();
-		store1.dispatch({ type: 'RESET' });
-	}
+	const counter = useSelector((state) => state);
+	const dispatch = useDispatch();
 
 	return (
 		<>
-			<button onClick={decrement}>Dec</button>
+			<button onClick={() => dispatch(decrement())}>Dec</button>
 			<span className='counter'>{counter}</span>
-			<button onClick={increment}>Inc</button>
+			<button onClick={() => dispatch(increment())}>Inc</button>
 			<br />
-			<button onClick={reset}>Reset</button>
+			<button onClick={() => dispatch(reset())}>Reset</button>
 		</>
 	);
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+	<Provider store={store1}>
+		<App />
+	</Provider>
+);
--- redux/store.js
+++ redux/store.js
@@ -1,14 +1,3 @@
-function reducer(currentState = 50, action) {
-	switch (action.type) {
-		case 'INC':
-			return currentState + action.payload;
-		case 'DEC':
-			return currentState - action.payload;
-		case 'RESET':
-			return 50;
-		default:
-			return currentState;
-	}
-}
+import { reducer } from './slice.js';
 
 export const store1 = Redux.createStore(reducer);
